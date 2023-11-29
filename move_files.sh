c0ab4100d9949725db94b8124bb86151d0565a36
Weigao Sun <sunweigao@outlook.com>
2023-11-29 22:59:30 +0800

    clean code

--- move_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#将一个目录下的一些文件移动到另一个目录下
-
-dir_figures="/Users/swg/Desktop/PLF/figures"  #可修改绝对路径；
-
-#mv ./*.eps /Users/swg/Weyun/iDirIMM/iDirIMM-tex.  #移动文件
-cp $dir_figures/* /Users/swg/Weyun/iDirIMM/iDirIMM-tex   #复制文件
--- test.py
+++
@@ -1,4 +0,0 @@
-df_4 = pd.read_csv('./wind_station_datasets/wind_station_4/95485-2008.csv', skiprows=3)
-wind_data_4 = df_4['power (MW)']
-wind_data_4 = wind_data_4.values
-wind_data_4 = wind_data_4.reshape(-1, 1)
